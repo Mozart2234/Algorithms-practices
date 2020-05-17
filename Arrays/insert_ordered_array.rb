63100c2a9dca49e0d4ea3f431f6e9e9adeae99ce
Alexei Mamani <alexeim763@gmail.com>
2020-05-17 12:11:46 -0500

    Refactor ruby mode

--- Arrays/insert_ordered_array.rb
+++ Arrays/insert_ordered_array.rb
@@ -1,6 +1,6 @@
 next_index = 5
 arr_example = [1, 2, 4, 5, 8, 10, nil, nil, nil, nil, nil]
-SEARCH = 3
+SEARCH = 7
 
 def search_secuencial_order(arr, next_index, search_value)
   i = 0
@@ -16,10 +16,9 @@ if next_index < 10
   else
     next_index += 1
     pos *= -1
-    next_index.downto(pos).each do |i|
-      arr_example[i] = arr_example[i - 1]
-    end
+    next_index.downto(pos).each { |i| arr_example[i] = arr_example[i - 1] }
     arr_example[pos] = SEARCH
   end
   p arr_example
+  p next_index
 end
